995c6bc623eedc77b0ff47e235552cfe604312cb
IkerLFernandezz <ikerfernandezlopeznuria@gmail.com>
2026-01-14 17:49:49 +0100

    fix(Persistance): InMemoryBookRepository error fixed

--- schem/src/Infrastructure/Persistence/InMemory/InMemoryBookRepository.php
+++ schem/src/Infrastructure/Persistence/InMemory/InMemoryBookRepository.php
@@ -3,20 +3,25 @@
 namespace App\Infrastructure\Persistance\InMemory;
 
 use App\Domain\Book\BookId;
+use App\Domain\Book\Book;
 use App\Domain\Book\IBookRepository;
 
-class InMemoryBookRepository implements IBookRepository{
+class InMemoryBookRepository implements IBookRepository
+{
     private array $books = [];
 
-    function save(Book $book): void{
-        $this->books[(string)$book->id()] = $book;
+    function saveBook(Book $book): void
+    {
+        $this->books[(string) $book->id()] = $book;
     }
 
-    function findById (BookId $id): ?Book{
-        return $this->books[(string)$id] ?? null;
+    function findById(BookId $id): ?Book
+    {
+        return $this->books[(string) $id] ?? null;
     }
 
-    function findAll (): array{
+    function findAll(): array
+    {
         return array_values($this->books);
     }
 
